852b12f16c75de148f21f7254c2f779c0844b43f
Anne LoVerso <anne.loverso@gmail.com>
2020-05-13 16:54:09 -0400

    WIP

--- pinelist-deployable/build.gradle.kts
+++ pinelist-deployable/build.gradle.kts
@@ -1,4 +1,7 @@
+import com.moowork.gradle.node.npm.NpmTask
+
 plugins {
+	id("com.github.node-gradle.node") version "2.2.0"
 	id("org.springframework.boot")
 	id("io.spring.dependency-management")
 	kotlin("jvm")
@@ -6,7 +9,7 @@ plugins {
 }
 
 tasks.register<Copy>("copyNpmBuild") {
-	dependsOn(":web:build")
+	//dependsOn(":web:build")
 	from("${projectDir}/../web/build")
 	into("src/main/resources/static")
 }
@@ -41,3 +44,52 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+
+//node {
+//	version = "13.6.0"
+//	npmVersion = "6.13.4"
+//	download = true
+//}
+
+val webDir = "${projectDir}/../web"
+
+//tasks.named<NpmTask>("npm_run_build") {
+//	inputs.files(fileTree("${webDir}/public"))
+//	inputs.files(fileTree("src"))
+//	inputs.file("package.json")
+//	inputs.file("package-lock.json")
+//
+//	outputs.dir("build")
+//}
+//
+//tasks.assemble {
+//	dependsOn("npm_run_build")
+//}
+//
+//val testsExecutedMarkerName: String = "${projectDir}/.tests.executed"
+//
+//tasks.registering(NpmTask::class) {
+//	dependsOn("assemble")
+//
+//	setEnvironment(mapOf("CI" to "true"))
+//	setArgs(listOf("run", "test"))
+//
+//	inputs.files(fileTree("src"))
+//	inputs.file("package.json")
+//	inputs.file("package-lock.json")
+//
+//	doLast {
+//		File(testsExecutedMarkerName).appendText("delete this file to force re-execution JavaScript tests")
+//	}
+//	outputs.file(testsExecutedMarkerName)
+//}
+//
+//tasks.check {
+//	dependsOn(test)
+//}
+//
+//tasks.clean {
+//	delete(testsExecutedMarkerName)
+//}
